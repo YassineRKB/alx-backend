0250d6f8b16055fdeca61e31a2b0eed52e2ed888
YassineRKB <yassine@arcraven.com>
2024-02-07 02:46:38 +0100

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for task 1"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["fr", "en"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hiMom():
+    """1-index.html"""
+    return render_template("1-index.html")
