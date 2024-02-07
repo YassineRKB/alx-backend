b2ba4cb4d703746b55b119cf97893725a1a92f0c
YassineRKB <yassine@arcraven.com>
2024-02-07 03:05:18 +0100

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for task 4"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["fr", "en"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """set locale"""
+    local = request.args.get("locale")
+    if local in Config.LANGUAGES:
+        return local
+    else:
+        local = request.accept_languages.best_match(app.config["LANGUAGES"])
+    return local
+
+
+@app.route("/")
+def helloMother():
+    """4-index.html"""
+    return render_template("4-index.html")
