58217e76acc95a0bdd4e188acba541346a3a3dde
YassineRKB <yassine@arcraven.com>
2024-03-06 22:49:32 +0100

    task 4

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+import { createClient } from "redis";
+import { promisify } from "util";
+const redis = require("redis");
+const client = createClient().on("error", (err) =>
+  console.log("Redis client not connected to the server:", err.message)
+);
+console.log("Redis client connected to the server");
+const Hasj = {
+  Portland: 50,
+  Seattle: 80,
+  "New York": 20,
+  Bogota: 20,
+  Cali: 40,
+  Paris: 2,
+};
+
+for (let key in Hasj) client.hmset("HolbertonSchools", key, Hasj[key], redis.print);
+
+client.hgetall("HolbertonSchools", (err, res) => console.log(res));
