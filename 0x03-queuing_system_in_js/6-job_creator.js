c7ba1ca17031b2a0240f477589b6053a91e8cba1
YassineRKB <yassine@arcraven.com>
2024-03-06 22:54:49 +0100

    task 6

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const kue = require("kue");
+const queue = kue.createQueue();
+const jobData = {
+  phoneNumber: "dads phone number",
+  message: "where's the milk?",
+};
+const job = queue
+  .create("push_notification_code", jobData)
+  .save((err) => {
+    if (!err) console.log("Notification job created:", job.id);
+  }).on("complete", () => console.log("Notification job completed"))
+  .on("failed", () => console.log("Notification job failed"));
