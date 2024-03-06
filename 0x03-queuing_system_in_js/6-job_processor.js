5f06cdf6abcd4741a7e84af5730c8b1ead5fbed5
YassineRKB <yassine@arcraven.com>
2024-03-06 22:56:56 +0100

    task 7

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import kue from "kue";
+const queue = kue.createQueue();
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+const someQueue = "push_notification_code";
+queue.process(someQueue, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
